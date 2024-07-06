7e7ba8702f7b9c2d13189277e823778732d7cab1
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-07-06 19:52:55 +0100

    SyntheticBaseEvent methodesD

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -2,18 +2,11 @@
 
 function Button (){
 
-    let count = 0 ;
-
-    const handelClick = (name) => {
-        if (count < 3){
-            count ++;
-            console.log(`${name} you clicked me ${count} time/s`)
-        }else{
-            console.log(`${name} stop clicking me!`)
-        }
-    };
+   
+    const handelClick = (e) => console.log(e)
+       
     
-    return(<button onClick={() => handelClick("sami")}> Click me </button>)
+    return(<button onClick={(e) => handelClick(e)}> Click me </button>)
 }
 
 export default Button
