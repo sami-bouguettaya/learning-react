023dd7a53239a75d0b5ddf6f08492dd1473518bf
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-07-10 19:37:31 +0100

    SyntheticBaseEvent

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,9 @@
+  function Button (){
+  
+    const handelClick = (e) => e.target.textContent = "OUCH !";
+
+    return(<button onClick={(e) => handelClick(e) }>Click me </button>)
+
+  }
+
+  export default Button
