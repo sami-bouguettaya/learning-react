87d74f1028a053db312fa86f784330248a8f6449
sami-bouguettaya <samibouguettaya66@gmail.com>
2024-07-10 19:16:32 +0100

    condition with ternary operator

--- my-react-app/src/UserGreeting.jsx
+++ my-react-app/src/UserGreeting.jsx
@@ -2,10 +2,10 @@
 
 function UserGreeting (props){
 
-    if (props.isLoggedIn) {
-        return( <h2> Welcome {props.username}</h2> )
-    }
-    return<h2>Please log in to continue </h2>
- }
+    return( props.isLoggedIn ?   <h2> Welcome {props.username}</h2> :
+                                <h2>Please log in to continue </h2>
+)
+
+}
  
  export default UserGreeting
